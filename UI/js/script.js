ccca6557ac7f13fefc4798b2692afaa8c02b6fb5
bmugenya26@gmail.com <bmugenya26@gmail.com>
2018-11-28 00:07:58 +0300

    [starts #162192280] modified layout

---
+++ UI/js/script.js
@@ -0,0 +1,7 @@
+function show() {
+    var x = document.getElementById("menu_nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+}
