320be3595cd0ca2fe47a02439bb3449a3804efa3
bmugenya26@gmail.com <bmugenya26@gmail.com>
2018-12-01 14:38:52 +0300

    [starts

--- tests/test_flag.py
+++ tests/test_flag.py
@@ -7,13 +7,13 @@ class TestproductResource(BaseCase):
 
     def test_can_create_incident(self):
 
-        response = self.client.post('record', json=self.data)
+        response = self.client.post('/api/v1/record', json=self.data)
         result = response.get_json()
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.get_json()['message'], 'Successfully created red flag')
 
     def test_can_get_incident(self):
-        response = self.client.get('record')
+        response = self.client.get('/api/v1/record')
         result = response.get_json()
         self.assertEqual(0, len(result["data"]))
         self.assertEqual(response.status_code, 200)
