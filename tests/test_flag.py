2cfa892c52108778018bc344b1be5a67db6bc914
Brian Mugenya <33225184+bmugenya@users.noreply.github.com>
2018-12-05 13:28:58 +0300

    Update test_flag.py

--- tests/test_flag.py
+++ tests/test_flag.py
@@ -7,7 +7,7 @@ class TestproductResource(BaseCase):
 
 
     def test_can_create_post(self):
-        response = self.client.post('/api/v1/record', json=self.data)
+        response = self.client.post('api/v1/record', json=self.data)
         result = response.get_json()
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.get_json()['message'], 'Successfully created red flag')
@@ -19,13 +19,13 @@ class TestproductResource(BaseCase):
         self.assertEqual(response.status_code, 200)
 
     def test_can_get_posts(self):
-        response = self.client.get('record')
+        response = self.client.get('api/v1/record')
         result = response.get_json()
         self.assertEqual(0, len(result["data"]))
         self.assertEqual(response.status_code, 200)
 
     def test_can_edit_post(self):
-        resp = self.client.patch('/api/v1//record/1')
+        resp = self.client.patch('/api/v1/record/1')
         self.assertEqual(resp.status_code, 200)
 
     def test_can_delete_post(self):
