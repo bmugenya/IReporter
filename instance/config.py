230f09dde2ce3e6d8d2b232ea1e32779e5a7f3b8
bmugenya26@gmail.com <bmugenya26@gmail.com>
2018-12-08 19:29:52 +0300

    [starts

--- instance/config.py
+++ instance/config.py
@@ -1,64 +1,22 @@
-# class Config(object):
-#     """Parent configuration class."""
-#     DEBUG = False
-#     CSRF_ENABLED = True
-
-
-# class DevelopmentConfig(Config):
-#     """Configurations for Development."""
-#     DEBUG = True
-
-
-# class TestingConfig(Config):
-#     """Configurations for Testing, with a separate test database."""
-#     TESTING = True
-#     DEBUG = True
-
-
-# class StagingConfig(Config):
-#     """Configurations for Staging."""
-#     DEBUG = True
-
-
-# class ProductionConfig(Config):
-#     """Configurations for Production."""
-#     DEBUG = False
-#     TESTING = False
-
-
-# app_config = {
-#     'development': DevelopmentConfig,
-#     'testing': TestingConfig,
-#     'staging': StagingConfig,
-#     'production': ProductionConfig,
-# }
-
-
-"""This module contains the various app configurations"""
 import os
 
 
 class Config():
-    """ Base config (other configs inherit from this base)"""
     DEBUG = False
-    SECRET = os.getenv('SECRET')
 
 
 class DevelopmentConfig(Config):
-    """Development Config"""
     DEBUG = True
     DATABASE_CONNECTION_URL = os.getenv('DATABASE_URL')
 
 
 class TestingConfig(Config):
-    """Testing Config"""
     TESTING = True
     DEBUG = True
     DATABASE_CONNECTION_URL = os.getenv('DATABASE_URL')
 
 
 class ProductionConfig(Config):
-    """Production Config"""
     DEBUG = False
     TESTIN = False
 
@@ -68,5 +26,3 @@ app_config = {
     'testing': TestingConfig,
     'production': ProductionConfig
 }
-
-secret_key = Config.SECRET
